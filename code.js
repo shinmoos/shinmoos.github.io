b2861f5fcc5f21e865f6485329a4bfa4ba3e1ac7
shinmoos <80040355+shinmoos@users.noreply.github.com>
2023-05-03 00:10:47 +0900

    commit

--- code.js
+++ code.js
@@ -1,8 +1,15 @@
 const youtubeLink = "https://www.youtube.com";
+const daumLink = "https://www.daum.net";
 
 const youtubeMenuItem = document.querySelector(".dropdown-content a:first-child");
+const daumMenuItem = document.querySelector(".dropdown-content a:nth-child(2)");
 
 youtubeMenuItem.addEventListener("click", function (event) {
   event.preventDefault();
   window.location.href = youtubeLink;
+});
+
+daumMenuItem.addEventListener("click", function (event) {
+  event.preventDefault();
+  window.location.href = daumLink;
 });
